c6c97fba9673846fde2e7d62be511fc95b561c52
ihebbennaceur <ihebbennaceur6@gmail.com>
2025-04-08 21:04:39 +0200

    models for users

---
+++ models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    jardins = relationship("Jardin", back_populates="owner")
+
+class Jardin(Base):
+    __tablename__ = "jardins"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, nullable=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="jardins")
